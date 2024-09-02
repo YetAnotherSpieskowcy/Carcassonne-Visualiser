5043fb2aa5efe3225682cfd7c59419b09667f73d
BognaLew <100587236+BognaLew@users.noreply.github.com>
2024-09-02 19:52:40 +0200

    created Widows make file

---
+++ make.ps1
@@ -0,0 +1,88 @@
+<#
+.Synopsis
+Makefile script in PowerShell that contains commands useful
+during development of Carcassonne-Visualiser.
+
+.Description
+Available commands:
+    run     Run application. Requires to pass log file name as param.
+    update  Get latest versions of Carcassonne-Engine and raylib-go.
+
+.Parameter Command
+Command to execute. See Cmdlet's description for more information.
+
+#>
+
+# I'm too dumb for PowerShell, so $script:availableCommands needs to be defined in 2 places // Jack
+
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    "PSReviewUnusedParameter",
+    "",
+    Justification = "Parameter is automatically provided by PowerShell which we have no control over."
+)]
+[CmdletBinding()]
+param (
+    [parameter(
+       ParameterSetName="run", 
+       Mandatory = $True, 
+       Position = 0)]
+    [parameter(
+       ParameterSetName="update", 
+       Mandatory = $True, 
+       Position = 0)]
+    [ValidateSet("run", "update")]
+    [String]
+    $command,
+    [parameter(
+       ParameterSetName="run", 
+       Mandatory = $True, 
+       Position = 1)]
+    [String]
+    $filename,
+    [switch]
+    $help = $false
+)
+
+function Exit-On-Fail([int]$exitCode) {
+    if ($exitCode) {
+        Exit $exitCode
+    }
+}
+
+function run() {
+    Write-Output "Running application..."
+    & go run . $filename -tags sdl 
+    Exit-ON-Fail $LASTEXITCODE
+}
+
+function update() {
+    Write-Output "Updating Carcassonne-Engine..."
+    & go get -u "github.com/YetAnotherSpieskowcy/Carcassonne-Engine@main"
+    Write-Output "Updating raylib-go"
+    & go get -v -u "github.com/gen2brain/raylib-go/raylib"
+    Exit-ON-Fail $LASTEXITCODE
+}
+
+$script:availableCommands = @(
+    "run",
+    "update"
+)
+
+if ($help) {
+    Get-Help $MyInvocation.InvocationName
+    exit
+}
+
+switch ($command) {
+    {$script:availableCommands -contains $_} {
+        & $command @Args
+        break
+    }
+    default {
+        Write-Output (
+            """$command"" is not a valid command.",
+            "To see available commands, type: ""$($MyInvocation.InvocationName) -help"""
+        )
+        break
+    }
+}
